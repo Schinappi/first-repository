c77e78f14d46c2641188d224efa647671d9d0e23
Schinappi <243764075@qq.com>
2020-02-14 12:31:43 +0800

    lala

--- src/main/java/haoyu/niubi/community/controller/AuthController.java
+++ src/main/java/haoyu/niubi/community/controller/AuthController.java
@@ -2,6 +2,8 @@ package haoyu.niubi.community.controller;
 
 import haoyu.niubi.community.dto.AccesstokenDTO;
 import haoyu.niubi.community.dto.GithubUser;
+import haoyu.niubi.community.mapper.UserMapper;
+import haoyu.niubi.community.model.User;
 import haoyu.niubi.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
-public class AuthController {
+     public class AuthController {
     @Autowired
     private GithubProvider githubProvider;
     @Value("${github.client.id}")
@@ -19,9 +25,12 @@ public class AuthController {
     private String client_secret;
     @Value("${github.redirect_uri}")
     private  String redirect_uri;
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
         accesstokenDTO.setCode(code);
         accesstokenDTO.setRedirect_uri(redirect_uri);
@@ -29,8 +38,20 @@ public class AuthController {
         accesstokenDTO.setClient_secret(client_secret);
         accesstokenDTO.setClient_id(client_id);
         String accessToken = githubProvider.getAccessToken(accesstokenDTO);
-        GithubUser user = githubProvider.getuser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getuser(accessToken);
+         if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccount_id(String.valueOf(githubUser.getId()));
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            user.setName(githubUser.getName());
+            userMapper.insert(user);
+        request.getSession().setAttribute("user",githubUser);
+        return "redirect:/";
+        }
+        else {
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/haoyu/niubi/community/mapper/UserMapper.java
+++ src/main/java/haoyu/niubi/community/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package haoyu.niubi.community.mapper;
 
-public class UserMapper {
+
+import haoyu.niubi.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+   @Insert("insert into user (ACCOUNT_ID,NAME,TOKEN,GMT_CREATE,GMT_MODIFIED)values(#{account_id},#{name},#{token},#{gmt_create},#{gmt_modified})")
+      void insert(User user);
 }
