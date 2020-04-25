f4ee19bacb2d1bf0e2154afec32c296ed4c7cdbc
Schinappi <243764075@qq.com>
2020-04-25 20:28:41 +0800

    end

---
+++ src/test/java/haoyu/niubi/community/Demo1ApplicationTests.java
@@ -0,0 +1,18 @@
+package haoyu.niubi.community;
+
+
+import haoyu.niubi.community.CommunityApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= CommunityApplication.class)
+public class Demo1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
